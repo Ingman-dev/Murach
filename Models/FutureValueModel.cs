1c8193f882b57f5f8b08e4ec2b18390592df4046
Ingman-dev <morten.ingman@yahoo.com>
2020-12-17 11:29:33 +0100

    Fixed syntax

--- Models/FutureValueModel.cs
+++ Models/FutureValueModel.cs
@@ -9,18 +9,18 @@ namespace Murach_lab1.Models
     public class FutureValueModel
     {   [Required(ErrorMessage ="Please enter a monthly investment amount.")]
         [Range(1, 500, ErrorMessage ="Monthly investment amount must be between 1 and 500.")]
-        public decimal MonthlyInvestment { get; set; }
+        public decimal? MonthlyInvestment { get; set; }
         [Required(ErrorMessage = "Please enter a yearly interest rate.")]
         [Range(0.1, 10.0, ErrorMessage = "Yearly interest rate must be between 0.1 and 10.")]
-        public decimal YearlyInterestRate { get; set; }
+        public decimal? YearlyInterestRate { get; set; }
         [Required(ErrorMessage = "Please enter the number of years.")]
         [Range(1, 50, ErrorMessage = "The number of years must be between 1 and 50.")]
-        public int Years { get; set; }
-        public decimal CalculateFutureValue()
+        public int? Years { get; set; }
+        public decimal? CalculateFutureValue()
         {
-            int months = Years * 12;
-            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
-            decimal futureValue = 0;
+            int? months = Years * 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal? futureValue = 0;
             for (int i = 0; i < months; i++)
             {
                 futureValue = (futureValue + MonthlyInvestment) *
